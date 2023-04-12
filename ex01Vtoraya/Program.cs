193ff60855d287112c3e12cabd8df32dfb1049ff
Arslan <arslan.chen@mail.ru>
2023-04-12 22:08:35 +0400

    ex01 function added

--- ex01Vtoraya/Program.cs
+++ ex01Vtoraya/Program.cs
@@ -1,10 +1,15 @@
 ﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.Write("Введите трехзначное число: ");
-int a = int.Parse(Console.ReadLine()!);
-while (a < 100 | a > 999)
+int Func1()
 {
-    Console.Write("Нужно трехзначное число, попробуйте еще раз: ");
-    a = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите трехзначное число: ");
+    int a = int.Parse(Console.ReadLine()!);
+    while (a < 100 | a > 999)
+    {
+        Console.Write("Нужно трехзначное число, попробуйте еще раз: ");
+        a = int.Parse(Console.ReadLine()!);
+    }
+    int b = a % 100 / 10;
+    return b;
 }
-int b = a % 100 / 10;
-Console.WriteLine("Вторая цифра во введенном числе - " + b);
+int number = Func1();
+Console.WriteLine("Вторая цифра во введенном числе - " + number);

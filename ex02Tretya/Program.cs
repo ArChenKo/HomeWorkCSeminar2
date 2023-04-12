496486230b0ef085b143124b8bbe15d9d15a3402
Arslan <arslan.chen@mail.ru>
2023-04-12 12:21:27 +0400

    ex02 red short added

--- ex02Tretya/Program.cs
+++ ex02Tretya/Program.cs
@@ -1,19 +1,12 @@
 ﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 Console.Write("Введите целое число: ");
 long a = long.Parse(Console.ReadLine()!);
-long b = 1000;
-long c = 1;
 if(a<100)
-{
     Console.Write("Третьей цифры нет.");
-}
 else
 {
-    while(a>=b)
-    {
-        c=c*10;
-        b=b*10;
-    }
-    long result = a/c%10;
+    while(a>999)
+        a/=10;
+    long result = a%10;
     Console.WriteLine(result);
 }
